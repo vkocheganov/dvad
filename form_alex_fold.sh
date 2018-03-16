a6d67f7f49f222dbac88fbcf185b6a81d2bd9450
vkocheganov <kocheganov@gmail.com>
2018-03-16 07:17:02 +0300

    add script file

---
+++ form_alex_fold.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf ./toalex/*
+cp ./src/*.cpp ./toalex/
+cp ./src/*.h ./toalex/
+cp ./calls-log-result-export.csv ./toalex/
+cp ./README ./toalex/
+cp ./run_prediction.py ./toalex/
+tar -zcvf ToAlex.tar.gz ./toalex/
